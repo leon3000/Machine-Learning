4e59fff0bc5982fcad2b91bd5d58534ef8abcf8b
guoxietao <289025881@qq.com>
2017-10-15 23:34:05 +0800

    20171015

---
+++ kaggle/Research/WSDM - KKBox's Churn Prediction Challenge/导图/KKBox's Churn Prediction Challenge.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1507967173947" ID="ID_1278845696" MODIFIED="1507967221186" TEXT="KKBox&apos;s Churn Prediction Challenge">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+</node>
+</map>
---
+++ kaggle/Research/WSDM - KKBox's Music Recommendation Challenge/导图/KKBox's Music Recommendation Challenge.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1507967245446" ID="ID_1028392736" MODIFIED="1507967250337" TEXT="KKBox&apos;s Music Recommendation Challenge">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+</node>
+</map>
