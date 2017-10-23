a008ca88480c628f69c4036f434c3522d07c4202
guoxietao <289025881@qq.com>
2017-10-23 15:58:51 +0800

    20171023

---
+++ 开源框架/机器学习框架.mm
@@ -0,0 +1,55 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1508724330246" ID="ID_183425659" MODIFIED="1508724358697" TEXT="&#x673a;&#x5668;&#x5b66;&#x4e60;&#x6846;&#x67b6;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1508724361271" ID="ID_1834367627" MODIFIED="1508724363934" POSITION="right" TEXT="Tensorflow">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1508724401778" ID="ID_1563115829" MODIFIED="1508724411450" POSITION="right" TEXT="Scikit-learn">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1508724364797" ID="ID_1095784642" MODIFIED="1508724375209" POSITION="right" TEXT="XGBoost">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508724912909" ID="ID_959619126" MODIFIED="1508724912911" TEXT="Scalable and Flexible Gradient Boosting">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508724375410" ID="ID_1001877313" MODIFIED="1508724379858" POSITION="right" TEXT="Keras">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1508724380334" ID="ID_235181698" MODIFIED="1508724386341" POSITION="right" TEXT="LightGBM">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1508724392801" ID="ID_1717256778" MODIFIED="1508724395126" POSITION="right" TEXT="CNTK">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508724954281" ID="ID_1626751453" MODIFIED="1508724954283" TEXT="Computational Network Toolkit">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508724397144" ID="ID_40911023" MODIFIED="1508724420403" POSITION="right" TEXT="Caffe">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1508724529474" ID="ID_641903392" MODIFIED="1508724690196" TEXT="Convolutional Architecture for Fast Feature Embedding &#x5377;&#x79ef;&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x6846;&#x67b6;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1508724420713" ID="ID_141097036" MODIFIED="1508724436291" POSITION="right" TEXT="DeepLeaning4j">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1508724498364" ID="ID_1068058332" MODIFIED="1508724500839" POSITION="right" TEXT="Theano">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
