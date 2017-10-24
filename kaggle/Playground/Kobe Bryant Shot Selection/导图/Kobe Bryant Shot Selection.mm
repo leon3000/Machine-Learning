04634dcdbdd1b22e66e20b98507b4d6475975186
guoxietao <289025881@qq.com>
2017-10-24 23:52:16 +0800

    20171024

--- kaggle/Playground/Kobe Bryant Shot Selection/导图/Kobe Bryant Shot Selection.mm
+++ kaggle/Playground/Kobe Bryant Shot Selection/导图/Kobe Bryant Shot Selection.mm
@@ -6,7 +6,27 @@
 <node COLOR="#0033ff" CREATED="1508763484697" ID="ID_330653628" MODIFIED="1508763486541" POSITION="right" TEXT="Preliminary exploration ">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1508764743962" ID="ID_1542950199" MODIFIED="1508764745488" TEXT="df.info()">
+<node COLOR="#00b439" CREATED="1508764743962" ID="ID_1542950199" MODIFIED="1508852993052" TEXT="&#x6295;&#x4e2d;shot_made_flag==1&#x4f4d;&#x7f6e;&#x5206;&#x5e03;&#x56fe;&#x548c;&#x6295;&#x5931;shot_made_flag==0&#x4f4d;&#x7f6e;&#x5206;&#x5e03;&#x56fe;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1508852027258" ID="ID_1477396254" MODIFIED="1508852960380" TEXT="&#x6295;&#x7bee;&#x7c7b;&#x578b;combined_shot_type&#x5206;&#x5e03;&#x56fe;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1508853021713" ID="ID_1975467060" MODIFIED="1508855100734" TEXT="&#x5bf9;&#x52a8;&#x4f5c;&#x7c7b;&#x578b;action_type&#x7684;&#x7b2c;&#x4e00;&#x4e2a;&#x5355;&#x8bcd;&#x8fdb;&#x884c;&#x5206;&#x7ec4;&#xff0c;&#x7136;&#x540e;&#x5206;&#x522b;&#x5bf9;&#x6bcf;&#x4e00;&#x7ec4;&#x7684;&#x5143;&#x7d20;&#x5728;&#x4e00;&#x5f20;&#x56fe;&#x4e0a;&#x505a;&#x6295;&#x4e2d;&#x548c;&#x6295;&#x5931;&#x4f4d;&#x7f6e;&#x5206;&#x5e03;&#x56fe;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1508853511585" ID="ID_1178156897" MODIFIED="1508855078807" TEXT="&#x5269;&#x4f59;&#x65f6;&#x95f4;&#x6295;&#x7bee;&#x5206;&#x5e03;&#x56fe;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1508856813420" ID="ID_1552714525" MODIFIED="1508856826641" TEXT="&#x6295;&#x4e2d;&#x6bd4;&#x4f8b;&#x8ddf;&#x968f;&#x8ddd;&#x79bb;&#x7684;&#x53d8;&#x5316;&#x66f2;&#x7ebf;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1508856827212" ID="ID_321074150" MODIFIED="1508856827212" TEXT="">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
