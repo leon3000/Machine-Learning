eeffc299b22b4e0d992df8f5014573a5578982ec
guoxietao <289025881@qq.com>
2017-10-28 00:17:00 +0800

    20171028

---
+++ 开源框架/sklearn/API Reference/API Reference.mm
@@ -0,0 +1,147 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1509117106747" ID="ID_537541676" MODIFIED="1509117160686" TEXT="API Reference">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1509117171384" ID="ID_357256379" MODIFIED="1509117183500" POSITION="right" TEXT="Base classes and utility functions">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117184360" ID="ID_1946301347" MODIFIED="1509117190543" POSITION="right" TEXT="Probability Calibration">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117191304" ID="ID_1713722212" MODIFIED="1509117193291" POSITION="right" TEXT="Clustering">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117199192" ID="ID_1561004756" MODIFIED="1509117203124" POSITION="right" TEXT="Biclustering">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117203320" ID="ID_520144259" MODIFIED="1509117208500" POSITION="right" TEXT="Convariance Estimators">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117214304" ID="ID_894440626" MODIFIED="1509117219953" POSITION="right" TEXT="Cross decomposition">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117220144" ID="ID_1545116937" MODIFIED="1509117222692" POSITION="right" TEXT="Datasets">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117228496" ID="ID_255328597" MODIFIED="1509117233155" POSITION="right" TEXT="Matrix Decomposition">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117233400" ID="ID_1829185273" MODIFIED="1509117245568" POSITION="right" TEXT="Discriminant Analysis">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117246288" ID="ID_1128103656" MODIFIED="1509117250938" POSITION="right" TEXT="Dummy estimators">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117251128" ID="ID_1013129211" MODIFIED="1509117255035" POSITION="right" TEXT="Ensemble Methods">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117259040" ID="ID_682670106" MODIFIED="1509117264563" POSITION="right" TEXT="Exceptions and warnings">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117269280" ID="ID_235299795" MODIFIED="1509117274265" POSITION="right" TEXT="Feature Extraction">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117279704" ID="ID_1212865288" MODIFIED="1509117282861" POSITION="right" TEXT="Feature selection">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117288176" ID="ID_833697705" MODIFIED="1509117293423" POSITION="right" TEXT="Gaussian Processses">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117293632" ID="ID_600871050" MODIFIED="1509117298407" POSITION="right" TEXT="Lsotonic regression">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117303864" ID="ID_1601549168" MODIFIED="1509117310125" POSITION="right" TEXT="Kernel Approximation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117310336" ID="ID_1155108194" MODIFIED="1509117315062" POSITION="right" TEXT="Kernel Ridge Regression">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117324760" ID="ID_1994871704" MODIFIED="1509117329814" POSITION="right" TEXT="Generalized Linear Modes">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117335216" ID="ID_284059079" MODIFIED="1509117339626" POSITION="right" TEXT="Manifold Learning">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117344080" ID="ID_1520401301" MODIFIED="1509117346345" POSITION="right" TEXT="Metrics">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117352176" ID="ID_1283302920" MODIFIED="1509117357110" POSITION="right" TEXT="Gaussian Mixtrue Modes">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117358744" ID="ID_1424896940" MODIFIED="1509117362188" POSITION="right" TEXT="Model Selection">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117367128" ID="ID_1220496399" MODIFIED="1509117379423" POSITION="right" TEXT="Multiclass and multiabel classification">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117383704" ID="ID_791678077" MODIFIED="1509117392439" POSITION="right" TEXT="Multioutput regression and classification">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117392632" ID="ID_1117170978" MODIFIED="1509117395931" POSITION="right" TEXT="Naive Bayes">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117401768" ID="ID_856283617" MODIFIED="1509117410298" POSITION="right" TEXT="Nearest Neighbors">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117410504" ID="ID_620321775" MODIFIED="1509117416235" POSITION="right" TEXT="Neural network models">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117417600" ID="ID_1837294277" MODIFIED="1509117422126" POSITION="right" TEXT="Pipeline">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117427376" ID="ID_849296612" MODIFIED="1509117434908" POSITION="right" TEXT="Preprocessing and Normalization">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117439824" ID="ID_747103850" MODIFIED="1509117444455" POSITION="right" TEXT="Random projection">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117444664" ID="ID_1114806603" MODIFIED="1509117452877" POSITION="right" TEXT="Semi-Supervised Learning">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117458112" ID="ID_1941000638" MODIFIED="1509117463990" POSITION="right" TEXT="Support Vector Machines">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117469976" ID="ID_1603699138" MODIFIED="1509117474111" POSITION="right" TEXT="Decision Trees">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509117474312" ID="ID_1002624818" MODIFIED="1509117477455" POSITION="right" TEXT="Utilities">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
