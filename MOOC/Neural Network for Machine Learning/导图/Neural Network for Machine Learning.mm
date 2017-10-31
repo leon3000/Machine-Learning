cabd1c83a5837bed4d3c412f26bf349b0644d7e7
guoxietao <289025881@qq.com>
2017-11-01 00:25:47 +0800

    20171101

---
+++ MOOC/Neural Network for Machine Learning/导图/Neural Network for Machine Learning.mm
@@ -0,0 +1,142 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1509465664557" ID="ID_888378736" MODIFIED="1509465672122" TEXT="Neural Network for Machine Learning">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1509465790951" ID="ID_1769888044" MODIFIED="1509465934497" POSITION="right" TEXT="1. Introduction">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1509466019090" ID="ID_1304034224" MODIFIED="1509466030663" TEXT="a.Why do we need machine learning">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1509466076450" ID="ID_96018504" MODIFIED="1509466085567" TEXT="b.what are neural networks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1509466137595" ID="ID_430510742" MODIFIED="1509466146198" TEXT="c.Some simle models of neurons">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1509466211467" ID="ID_953615287" MODIFIED="1509466216023" TEXT="Linear neurons">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1509466216339" ID="ID_642928686" MODIFIED="1509466232103" TEXT="Binary threshold neurons">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1509466248970" ID="ID_308862323" MODIFIED="1509466267071" TEXT="Rectified Linear Neurons(Linear threswhold neurons)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1509466274978" ID="ID_207795386" MODIFIED="1509466285919" TEXT="Sigmoid neurons">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1509466340090" ID="ID_545926471" MODIFIED="1509466347351" TEXT="Stochastic binary neurons">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1509466356026" ID="ID_1601803210" MODIFIED="1509466366271" TEXT="d.A simple example of learning">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1509466410794" ID="ID_123707423" MODIFIED="1509466416815" TEXT="e.Three types of learning">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1509466428546" ID="ID_796967106" MODIFIED="1509466436655" TEXT="Supervised learning">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1509466452938" ID="ID_1280984202" MODIFIED="1509466455774" TEXT="Regression"/>
+<node COLOR="#111111" CREATED="1509466455970" ID="ID_808552921" MODIFIED="1509466458519" TEXT="Classification"/>
+</node>
+<node COLOR="#990000" CREATED="1509466436858" ID="ID_1228208399" MODIFIED="1509466442231" TEXT="Reinforcement learning">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1509466442467" ID="ID_1821511345" MODIFIED="1509466446703" TEXT="Unsupervised learning">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1509465797742" ID="ID_992258904" MODIFIED="1509465925165" POSITION="right" TEXT="2.The Perceptron learning procedure">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1509466511138" ID="ID_775292487" MODIFIED="1509466528111" TEXT="a.An overview of the amin types of neural network architecture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1509466576850" ID="ID_666919396" MODIFIED="1509466588711" TEXT="Feed-forward neural networks">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1509466588906" ID="ID_1329802007" MODIFIED="1509466597511" TEXT="Recurrent networks">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1509466611331" ID="ID_1185620250" MODIFIED="1509466621607" TEXT="Recurrent neural networks for modeling sequences">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1509466638362" ID="ID_1587112462" MODIFIED="1509466652727" TEXT="b.Perceptrons:The first generation of neural networks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1509466700106" ID="ID_115108090" MODIFIED="1509466709983" TEXT="c. A geometrical veiw of perceptrons">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1509466870370" ID="ID_1783033769" MODIFIED="1509466877681" TEXT="d.Why the learing works">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1509467083826" ID="ID_1788133770" MODIFIED="1509467092871" TEXT="e.What perceptrons can not do">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1509465802351" ID="ID_1758567850" MODIFIED="1509465938990" POSITION="right" TEXT="3.The backpropagation learning procedure">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465807790" ID="ID_1470664774" MODIFIED="1509465944125" POSITION="right" TEXT="4.Learning feature vectors for words">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465812397" ID="ID_1602542841" MODIFIED="1509465946708" POSITION="right" TEXT="5.Object recognition with neural nets">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465816205" ID="ID_1028577915" MODIFIED="1509465949421" POSITION="right" TEXT="6.Optimization: How to make the learning go faster">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465833626" ID="ID_1898834469" MODIFIED="1509465952424" POSITION="right" TEXT=" 7.Recurrent neural networks">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465847916" ID="ID_308229021" MODIFIED="1509465955251" POSITION="right" TEXT="8.More recurrent neural networks">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465862475" ID="ID_1966281805" MODIFIED="1509465958020" POSITION="right" TEXT="9.Ways to make neural networks generalize better">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465871811" ID="ID_1831336580" MODIFIED="1509465970401" POSITION="right" TEXT="10.Combining multiple neural networks to improve generalization">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465873275" ID="ID_1175134678" MODIFIED="1509465974098" POSITION="right" TEXT="11.Hopfield nets and Boltzmann machines">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465881178" ID="ID_1204339492" MODIFIED="1509465976479" POSITION="right" TEXT="12.Restricted Boltzmann machines (RBMs)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465886867" ID="ID_882173771" MODIFIED="1509465980384" POSITION="right" TEXT="13.Stacking RBMs to make Deep Belief Nets">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465892659" ID="ID_1624958301" MODIFIED="1509465989699" POSITION="right" TEXT="14.Deep neural nets with generative pre-training">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509465897650" ID="ID_933439705" MODIFIED="1509465997074" POSITION="right" TEXT="15.Modeling hierarchical structure with neural nets">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
