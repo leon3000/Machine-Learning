988fbae4bd9c83a2c334ce78fb2ea6d9730fd33c
guoxietao <289025881@qq.com>
2017-11-01 16:30:24 +0800

    20171101

---
+++ MOOC/Stanford University CS231n/导图/CS231n-深度学习计算机视觉.mm
@@ -0,0 +1,59 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1509524234063" ID="ID_1521731999" MODIFIED="1509524338160" TEXT="CS231n-&#x6df1;&#x5ea6;&#x5b66;&#x4e60;&#x8ba1;&#x7b97;&#x673a;&#x89c6;&#x89c9;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1509524366085" ID="ID_365129114" MODIFIED="1509524591654" POSITION="right" TEXT="&#x8ba1;&#x7b97;&#x673a;&#x89c6;&#x89c9;&#x5386;&#x53f2;&#x56de;&#x987e;&#x4e0e;&#x4ecb;&#x7ecd;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524591845" ID="ID_1480421107" MODIFIED="1509524607671" POSITION="right" TEXT="&#x6570;&#x636e;&#x9a71;&#x52a8;&#x7684;&#x56fe;&#x5f62;&#x5206;&#x88c2;&#x65b9;&#x5f0f;&#xff1a;K&#x6700;&#x8fd1;&#x90bb;&#x4e0e;&#x7ebf;&#x6027;&#x5206;&#x7c7b;&#x5668;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524609200" ID="ID_274240350" MODIFIED="1509524621028" POSITION="right" TEXT="&#x7ebf;&#x6027;&#x5206;&#x7c7b;&#x5668;&#x635f;&#x5931;&#x51fd;&#x6570;&#x4e0e;&#x6700;&#x4f18;&#x5316;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524628333" ID="ID_54877351" MODIFIED="1509524640132" POSITION="right" TEXT="&#x53cd;&#x5411;&#x4f20;&#x64ad;&#x4e0e;&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x521d;&#x6b65;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524640316" ID="ID_107561018" MODIFIED="1509524645360" POSITION="right" TEXT="&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x8bad;&#x7ec3;&#x7ec6;&#x8282;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524646670" ID="ID_1851351724" MODIFIED="1509524652441" POSITION="right" TEXT="&#x5377;&#x79ef;&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x8be6;&#x89e3;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524661011" ID="ID_212888025" MODIFIED="1509524674895" POSITION="right" TEXT="&#x8fc1;&#x79fb;&#x5b66;&#x4e60;&#x4e4b;&#x7269;&#x4f53;&#x5b9a;&#x4f4d;&#x4e0e;&#x68c0;&#x6d4b;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524675113" ID="ID_666112976" MODIFIED="1509524687975" POSITION="right" TEXT="&#x5377;&#x79ef;&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x7684;&#x53ef;&#x89c6;&#x5316;&#x4e0e;&#x8fdb;&#x4e00;&#x6b65;&#x7406;&#x89e3;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524688179" ID="ID_1692356769" MODIFIED="1509524691466" POSITION="right" TEXT="&#x5faa;&#x73af;&#x795e;&#x7ecf;&#x7f51;&#x7edc;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524691649" ID="ID_692854807" MODIFIED="1509524702197" POSITION="right" TEXT="&#x5377;&#x79ef;&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x5de5;&#x7a0b;&#x5b9e;&#x8df5;&#x6280;&#x5de7;&#x4e0e;&#x6ce8;&#x610f;&#x70b9;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524702398" ID="ID_1727322466" MODIFIED="1509524710549" POSITION="right" TEXT="&#x6df1;&#x5ea6;&#x5b66;&#x4e60;&#x5f00;&#x6e90;&#x5e93;&#x4f7f;&#x7528;&#x4ecb;&#x7ecd;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524716517" ID="ID_989369320" MODIFIED="1509524725093" POSITION="right" TEXT="&#x56fe;&#x5f62;&#x5206;&#x5272;&#x4e0e;&#x6ce8;&#x610f;&#x529b;&#x6a21;&#x578b;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509524725416" ID="ID_808672387" MODIFIED="1509524730894" POSITION="right" TEXT="&#x89c6;&#x9891;&#x68c0;&#x6d4b;&#x4e0e;&#x65e0;&#x76d1;&#x7763;&#x5b66;&#x4e60;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
