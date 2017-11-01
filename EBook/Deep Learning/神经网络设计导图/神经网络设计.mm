c63b91f2e7418ce1615c5f2b449d430278c9d300
guoxietao <289025881@qq.com>
2017-11-01 10:21:53 +0800

    20171101

---
+++ EBook/Deep Learning/神经网络设计导图/神经网络设计.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1509502669563" ID="ID_1138902479" MODIFIED="1509502675956" TEXT="&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x8bbe;&#x8ba1;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+</node>
+</map>
