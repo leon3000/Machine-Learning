4c27cf83d7a52a866670788b2d6cb91358319a25
guoxietao <289025881@qq.com>
2017-11-03 16:28:14 +0800

    20171103

---
+++ Kaggle/Competitions.mm
@@ -0,0 +1,111 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1509697486308" ID="ID_1003708897" MODIFIED="1509697604093" TEXT="Competitions">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1509697509206" ID="ID_1059624954" MODIFIED="1509697604084" POSITION="right" TEXT="Featured">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509697571545" ID="ID_700662396" MODIFIED="1509697604087" POSITION="right" TEXT="Research">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1509697587739" ID="ID_1913011107" MODIFIED="1509697604090" POSITION="right" TEXT="Getting Started">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1505100235787" ID="ID_1121604919" MODIFIED="1509697643988" TEXT="Titanic: Machine Learning from Disaster">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1505201417118" ID="ID_1502890474" MODIFIED="1509697643989" TEXT="Practice Skills">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1505100237344" ID="ID_289687820" MODIFIED="1509697643989" TEXT="Binary classification">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1505100249430" ID="ID_1401204327" MODIFIED="1509697643990" TEXT="Python and R basics">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1505283444210" ID="ID_1779538804" MODIFIED="1509697643993" TEXT="A Journey through Titanic ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1505283445172" ID="ID_1741219370" MODIFIED="1509697643993" TEXT="Introduction to Ensembling/Stacking in Python ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1505283480276" ID="ID_1791820160" MODIFIED="1509697643996" TEXT="Titanic Data Science Solutions ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1505283494330" ID="ID_1774292664" MODIFIED="1509697643997" TEXT="Pytanic ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1505100200003" ID="ID_1283977899" MODIFIED="1509697649522" TEXT="House Prices: Advanced Regression Techniques">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1505181073690" ID="ID_502931447" MODIFIED="1509697649523" TEXT="Practice Skills">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1505100266412" ID="ID_1620872825" MODIFIED="1509697649526" TEXT="Creative feature engineering">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1505100267617" ID="ID_1955342" MODIFIED="1509697649528" TEXT="Advanced regression techniques like random forest and gradient boostin">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1505181037218" ID="ID_1610246615" MODIFIED="1509697649529" TEXT="Comprehensive data exploration with Python ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1505181088370" ID="ID_1890051593" MODIFIED="1509697649530" TEXT="zoomed heatmap style">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1505181103362" ID="ID_1735515007" MODIFIED="1509697649531" TEXT="move like Jagger style">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1505283640616" ID="ID_1705919293" MODIFIED="1509697649532" TEXT="Regularized Linear Models ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1505283641637" ID="ID_678142703" MODIFIED="1509697649533" TEXT="Stacked Regressions : Top 4% on LeaderBoard ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1505100306037" ID="ID_1627482252" MODIFIED="1509697655604" TEXT="Digit Recognizer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1505201419559" ID="ID_1993242217" MODIFIED="1509697655605" TEXT="Practice Skills">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1505100306998" ID="ID_1845890753" MODIFIED="1509697655606" TEXT="Computer vision fundamentals including simple neural networks">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1505100318898" ID="ID_441590370" MODIFIED="1509697655607" TEXT="Classification methods such as SVM and K-nearest neighbors">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1505283745420" ID="ID_429603869" MODIFIED="1509697655608" TEXT="TensorFlow deep NN ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1505283746033" ID="ID_1739898807" MODIFIED="1509697655609" TEXT="Interactive Intro to Dimensionality Reduction ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1505283757132" ID="ID_1082610863" MODIFIED="1509697655616" TEXT="Deep Neural Network Keras way ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1509697591672" ID="ID_535963644" MODIFIED="1509697604093" POSITION="right" TEXT="Playground">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
