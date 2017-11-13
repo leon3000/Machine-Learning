3b93ad24742ff2b327236606b14a955c01c5b3f3
guoxietao <289025881@qq.com>
2017-11-13 16:59:40 +0800

    20171113

---
+++ Frameworks/seaborn/seaborn.mm
@@ -0,0 +1,108 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1510562013531" ID="ID_1718343874" MODIFIED="1510562227360" TEXT="http://seaborn.pydata.org/index.html">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1510562097017" ID="ID_14859368" MODIFIED="1510562109019" POSITION="right" TEXT="Example gallery">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1510562078435" ID="ID_328744333" MODIFIED="1510562087124" POSITION="right" TEXT="tutorial">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1510562124910" ID="ID_1920142922" MODIFIED="1510562124916" TEXT="Style management">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1510562153250" ID="ID_1297109700" MODIFIED="1510562153255" TEXT="Controlling figure aesthetics">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1510562156444" ID="ID_778945365" MODIFIED="1510562156446" TEXT="Choosing color palettes">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1510562129255" ID="ID_894499710" MODIFIED="1510562129257" TEXT="Plotting functions">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1510562166458" ID="ID_1909708064" MODIFIED="1510562323537" TEXT="Visualizing the distribution of a dataset">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1510562271095" ID="ID_1194453080" MODIFIED="1510562271096" TEXT="Plotting univariate distributions"/>
+<node COLOR="#111111" CREATED="1510562278803" ID="ID_1936381324" MODIFIED="1510562278804" TEXT="Plotting bivariate distributions"/>
+<node COLOR="#111111" CREATED="1510562283458" ID="ID_1980427164" MODIFIED="1510562283459" TEXT="Visualizing pairwise relationships in a dataset"/>
+</node>
+<node COLOR="#990000" CREATED="1510562169576" ID="ID_1235848507" MODIFIED="1510562316724" TEXT="Plotting with categorical data">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1510562329591" ID="ID_1793757755" MODIFIED="1510562329592" TEXT="Categorical scatterplots"/>
+<node COLOR="#111111" CREATED="1510562333039" ID="ID_1320863917" MODIFIED="1510562333042" TEXT="Categorical scatterplots"/>
+<node COLOR="#111111" CREATED="1510562335776" ID="ID_863163822" MODIFIED="1510562335776" TEXT="Statistical estimation within categories"/>
+<node COLOR="#111111" CREATED="1510562338460" ID="ID_187310788" MODIFIED="1510562338461" TEXT="Plotting &#x201c;wide-form&#x201d; data"/>
+<node COLOR="#111111" CREATED="1510562341341" ID="ID_1550532803" MODIFIED="1510562341342" TEXT="Drawing multi-panel categorical plots"/>
+</node>
+<node COLOR="#990000" CREATED="1510562172382" ID="ID_614021955" MODIFIED="1510562172383" TEXT="Visualizing linear relationships">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1510562363183" ID="ID_1268167351" MODIFIED="1510562363184" TEXT="Functions to draw linear regression models"/>
+<node COLOR="#111111" CREATED="1510562367053" ID="ID_502804288" MODIFIED="1510562367053" TEXT="Fitting different kinds of models"/>
+<node COLOR="#111111" CREATED="1510562371872" ID="ID_1283065841" MODIFIED="1510562371873" TEXT="Conditioning on other variables"/>
+<node COLOR="#111111" CREATED="1510562376024" ID="ID_83784422" MODIFIED="1510562376025" TEXT="Controlling the size and shape of the plot"/>
+<node COLOR="#111111" CREATED="1510562379392" ID="ID_575549428" MODIFIED="1510562379393" TEXT="Plotting a regression in other contexts"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1510562133781" ID="ID_446845111" MODIFIED="1510562133784" TEXT="Structured grids">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1510562177729" ID="ID_1974520631" MODIFIED="1510562177731" TEXT="Plotting on data-aware grids">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1510562196342" ID="ID_1101008645" MODIFIED="1510562196343" TEXT="Plotting small multiples of data subsets"/>
+<node COLOR="#111111" CREATED="1510562200131" ID="ID_1821988325" MODIFIED="1510562200132" TEXT="Plotting pairwise relationships in a dataset"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1510562087771" ID="ID_1089269264" MODIFIED="1510562093053" POSITION="right" TEXT="API reference">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1510563502045" ID="ID_366467559" MODIFIED="1510563502064" TEXT="Axis grids">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563507786" ID="ID_21473529" MODIFIED="1510563507796" TEXT="Categorical plots">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563514036" ID="ID_598533360" MODIFIED="1510563514042" TEXT="Distribution plots">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563519292" ID="ID_1964690969" MODIFIED="1510563519295" TEXT="Regression plots">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563522472" ID="ID_1910624124" MODIFIED="1510563522489" TEXT="Matrix plots">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563526738" ID="ID_1769985076" MODIFIED="1510563526742" TEXT="Timeseries plots">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563531209" ID="ID_1635441122" MODIFIED="1510563531212" TEXT="Miscellaneous plots">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563534968" ID="ID_232074509" MODIFIED="1510563534970" TEXT="Style frontend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563539177" ID="ID_1939369710" MODIFIED="1510563539180" TEXT="Color palettes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563543779" ID="ID_794285388" MODIFIED="1510563543782" TEXT="Palette widgets">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510563548808" ID="ID_1823072381" MODIFIED="1510563548832" TEXT="Utility functions">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
