c3465dea85cc3d6646b46d6716bb6c38960fdb5b
guoxietao <289025881@qq.com>
2017-11-14 00:09:24 +0800

    20171114

--- kaggle/Going/Text Normalization Challenge - English Language.mm
+++ kaggle/Going/Text Normalization Challenge - English Language.mm
@@ -9,10 +9,17 @@
 <node COLOR="#00b439" CREATED="1510587552126" ID="ID_1496582941" MODIFIED="1510587556650" TEXT="&#x6570;&#x636e;&#x548c;&#x7279;&#x5f81;&#x5de5;&#x7a0b;">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1510577426069" ID="ID_1305265934" MODIFIED="1510587703918" TEXT="Watch your language - update: feature engineering">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 <node COLOR="#00b439" CREATED="1510587556845" ID="ID_1766480149" MODIFIED="1510587563018" TEXT="&#x6a21;&#x578b;&#x548c;&#x7b97;&#x6cd5;">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1510577277779" ID="ID_1174256410" MODIFIED="1510587709398" TEXT="XGboost With Context Label Data (ACC:  99.637%)">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1510577304299" ID="ID_996395009" MODIFIED="1510577314401" TEXT="XGboost +context"/>
+</node>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1510587564133" ID="ID_1327939373" MODIFIED="1510587567569" POSITION="right" TEXT="Discussion">
