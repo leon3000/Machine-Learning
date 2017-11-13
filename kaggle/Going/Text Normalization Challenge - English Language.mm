7bcd227b9aa7cf47a68f4b2c0038311e9c82df63
guoxietao <289025881@qq.com>
2017-11-13 23:40:11 +0800

    20171113

---
+++ kaggle/Going/Text Normalization Challenge - English Language.mm
@@ -0,0 +1,23 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1510587528550" ID="ID_1068940712" MODIFIED="1510587545413" TEXT="Text Normalization Challenge - English Language">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1510587548606" ID="ID_994475241" MODIFIED="1510587551010" POSITION="right" TEXT="Kernel">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1510587552126" ID="ID_1496582941" MODIFIED="1510587556650" TEXT="&#x6570;&#x636e;&#x548c;&#x7279;&#x5f81;&#x5de5;&#x7a0b;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1510587556845" ID="ID_1766480149" MODIFIED="1510587563018" TEXT="&#x6a21;&#x578b;&#x548c;&#x7b97;&#x6cd5;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1510587564133" ID="ID_1327939373" MODIFIED="1510587567569" POSITION="right" TEXT="Discussion">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
